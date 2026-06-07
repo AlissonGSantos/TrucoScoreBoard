6f849b0fe8242b43a8f9fae819cc82188edf4b88
AlissonGSantos <alisson.gab.santos@gmail.com>
2026-06-06 21:03:02 -0300

    feat: Implement scoring logic and win history activity

---
+++ app/src/main/java/br/edu/utfpr/trucoscoreboard/HistoryActivity.kt
@@ -0,0 +1,34 @@
+package br.edu.utfpr.trucoscoreboard
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.utfpr.trucoscoreboard.databinding.ActivityHistoryBinding
+
+class HistoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHistoryBinding
+    private var firstPlayerWins = 0
+    private var secondPlayerWins = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.tvFirstPlayerWins.text = intent.getIntExtra("first_player_wins", 0).toString()
+        binding.tvSecondPlayerWins.text = intent.getIntExtra("second_player_wins", 0).toString()
+
+        binding.btnGoBack.setOnClickListener { finish() }
+
+    }
+}
--- app/src/main/java/br/edu/utfpr/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/trucoscoreboard/MainActivity.kt
@@ -1,15 +1,21 @@
 package br.edu.utfpr.trucoscoreboard
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.utfpr.trucoscoreboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+    private var firstPlayerScore = 0
+    private var secondPlayerScore = 0
+    private var firstPlayerWins = 0
+    private var secondPlayerWins = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -25,13 +31,93 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (savedInstanceState != null) {
-            binding.tvFirstPlayerPoints.text = savedInstanceState.getString("first_player_points")
-            binding.tvSecondPlayerPoints.text = savedInstanceState.getString("second_player_points")
+            firstPlayerScore = savedInstanceState.getInt("first_player_score")
+            secondPlayerScore = savedInstanceState.getInt("second_player_score")
+            firstPlayerWins = savedInstanceState.getInt("first_player_wins")
+            secondPlayerWins = savedInstanceState.getInt("second_player_wins")
+            updateUI()
         } else {
-            binding.tvFirstPlayerPoints.text = getString(R.string.player_points)
-            binding.tvSecondPlayerPoints.text = getString(R.string.player_points)
+            updateUI()
         }
 
+        setupPlayerButtons(1, binding.btn1FirstPlayer, binding.btn3FirstPlayer, binding.btn6FirstPlayer, binding.btn9FirstPlayer, binding.btn12FirstPlayer)
+        setupPlayerButtons(2, binding.btn1SecondPlayer, binding.btn3SecondPlayer, binding.btn6SecondPlayer, binding.btn9SecondPlayer, binding.btn12SecondPlayer)
+        binding.btnClearHistory.setOnClickListener { resetWins() }
+        binding.btnHistory.setOnClickListener { showHistoryActivity() }
+        binding.btnChangeName.setOnClickListener { showChangeNameActivity()  }
+    }
 
+    private fun showChangeNameActivity() {
+// TODO: implement change name view 
+    }
+
+    private fun showHistoryActivity() {
+        val intent = Intent(this, HistoryActivity::class.java)
+
+        intent.putExtra("first_player_wins", firstPlayerWins)
+        intent.putExtra("second_player_wins", secondPlayerWins)
+
+        startActivity(intent)
+    }
+
+    private fun setupPlayerButtons(player: Int, vararg buttons: View) {
+        val points = listOf(1, 3, 6, 9, 12)
+        buttons.forEachIndexed { index, button ->
+            button.setOnClickListener { addPoints(player, points[index]) }
+        }
+    }
+
+    private fun addPoints(player: Int, points: Int) {
+        when (player) {
+            1 -> firstPlayerScore += points
+            2 -> secondPlayerScore += points
+        }
+        updateUI()
+        checkWinner()
+    }
+
+    private fun checkWinner() {
+        if (firstPlayerScore >= 12) {
+            firstPlayerWins++
+            showWinnerDialog(getString(R.string.player_1_wins))
+        } else if (secondPlayerScore >= 12) {
+            secondPlayerWins++
+            showWinnerDialog(getString(R.string.player_2_wins))
+        }
+    }
+
+    private fun showWinnerDialog(message: String) {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.winner_dialog_title)
+            .setMessage(message)
+            .setPositiveButton(R.string.ok) { _, _ ->
+                resetPoints()
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun updateUI() {
+        binding.tvFirstPlayerPoints.text = getString(R.string.player_points, firstPlayerScore.toString())
+        binding.tvSecondPlayerPoints.text = getString(R.string.player_points, secondPlayerScore.toString())
+    }
+
+    private fun resetPoints() {
+        firstPlayerScore = 0
+        secondPlayerScore = 0
+        updateUI()
+    }
+
+    private fun resetWins() {
+        firstPlayerWins = 0
+        secondPlayerWins = 0
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("first_player_score", firstPlayerScore)
+        outState.putInt("second_player_score", secondPlayerScore)
+        outState.putInt("first_player_wins", firstPlayerWins)
+        outState.putInt("second_player_wins", secondPlayerWins)
     }
 }
